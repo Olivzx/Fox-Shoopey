95dea8e74484f8271618c1e57e8d824c8e674347
Carliinhos | Dev <carloshenrique.mo19@gmail.com>
2026-09-25 11:25:25 -0300

    chore: configure public Supabase client

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Esta chave é pública e pode ficar no navegador.
 // NUNCA coloque aqui uma service_role/secret key.
 window.FOX_SUPABASE = {
-  url: '',
-  publishableKey: ''
+  url: 'https://xotsezpsvtmofrmtfflq.supabase.co',
+  publishableKey: 'sb_publishable_u8dB50UuUyMn8x1xhWmAvg_5kSKqkMq'
 };
